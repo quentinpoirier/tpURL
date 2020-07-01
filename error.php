7445c1f44933214b2b36fe3f21107ee09d6b7523
unknown <quentin-poirier@live.fr>
2020-07-01 15:32:57 +0200

    création d'une page error 404 via error.php

---
+++ error.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>erreur 404</title>
+</head>
+<body>
+<h1>Oups! Page introuvable</h1>
+<p>Vous avez tenté d’accéder à une page qui ne se trouve plus sur ce site. Veuillez revenir sur la page d'accueil afin de continuer votre navigation sur notre site.</p>
+<p><a href="page1.html">Revenir à l'accueil</a></p>
+</body>
+</html>
