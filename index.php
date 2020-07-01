ce067c4ee4cd4a3ccee697a39138592777c20493
unknown <quentin-poirier@live.fr>
2020-07-01 11:03:55 +0200

    affichage page fonction de l'id

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
 <?php
 if (file_exists('source.xml')) {
-    $xml = simplexml_load_file('source.xml');
-    echo($xml);
+    $sourceXml = simplexml_load_file('source.xml');
+    echo ($sourceXml);
 } else {
     exit('Echec lors de l\'ouverture du fichier test.xml.');
-} 
-var_dump($xml);
-
-foreach ($xml as $section) {
-    echo $section->content;
 }
+var_dump($sourceXml);
 
+// echo $sourceXml->page[1] ->content;
+foreach ($sourceXml->page as $page) {
+    echo '<a href="index.php?id=' . (intval($page[('id')]) - 1) . '">accueil</a>';
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TP XML</title>
 </head>
+
 <body>
-    
+    <nav>
+        <a href="index.php?">Accueil</a>
+    </nav>
+    <?php echo $sourceXml->page[intval($_GET['id'])]->content; ?>
 </body>
+
 </html>
