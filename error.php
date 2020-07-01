9f85060aa40297156b0f4dc8517eac33093f2f93
unknown <quentin-poirier@live.fr>
2020-07-01 16:44:41 +0200

    visuel très kitch sur error.php

--- error.php
+++ error.php
@@ -1,13 +1,38 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <title>erreur 404</title>
+    <style>
+        .bgBlack {
+            background-color: #000000;
+        }
+        .textColorRed {
+            color: red;
+        }
+    </style>
 </head>
+
 <body>
-<h1>Oups! Page introuvable</h1>
-<p>Vous avez tenté d’accéder à une page qui ne se trouve plus sur ce site. Veuillez revenir sur la page d'accueil afin de continuer votre navigation sur notre site.</p>
-<p><a href="page1.html">Revenir à l'accueil</a></p>
+    <div class="bgBlack">
+        <div class="row justify-content-center">
+            <div class="col-sm-6 mt-3 mb-3">
+                <div class="h2 text-center textColorRed text-uppercase">Tu t'égare camarade !</div>
+                <div class="text-center"><img class="w-100" src="assets\img\iseeyou.gif"></div>
+                <div class="h5 text-center textColorRed">Vous avez tenté d’accéder à une page qui ne se trouve pas sur ce site. Veuillez revenir sur la page d'accueil afin de continuer votre navigation sur notre site.</div>
+                <div class='text-center'><a class="h3 textColorRed text-uppercase" href="page1.html">retourne dans le droit chemin</a></div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
+
 </html>
